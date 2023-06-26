b98204581bec32b2167775c409471c23ac589700
amielnoy <amielnoy@gmail.com>
2023-06-27 00:26:45 +0300

    final main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
-#provider "azurerm" {
-#  features {}
-#
-#  subscription_id = "441b9518-6510-4fa6-9882-fa9d2d75513e"
-#  client_id       = "5dc776b1-1400-4044-bba6-98dfe595d9b7"
-#  client_secret   = "K1x8Q~hCLAGnNXvx66D8Jjz0WQgwBDBgAOjGtbXu"
-#  tenant_id       = "eb942138-7243-491f-8654-718908188a40"
-#}
-
 resource "azurerm_resource_group" "amiel_az_resource" {
-  name     = "amiel-resource-group"
+  name     = "amiel-resource-group2"
   location = "West US"
 }
 
@@ -34,8 +25,8 @@ resource "azurerm_storage_account" "amiel_storage" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_app_service_plan" "azure_amiel_serice_plan" {
-  name                = "amiel_app_service_plan"
+resource "azurerm_app_service_plan" "azure_amiel_service_plan" {
+  name                = "amiel-app-service-plan"
   location            = azurerm_resource_group.amiel_az_resource.location
   resource_group_name = azurerm_resource_group.amiel_az_resource.name
   kind                = "FunctionApp"
@@ -49,7 +40,7 @@ resource "azurerm_function_app" "amiel_az_func_app" {
   name                = "amiel-function-app"
   location            = azurerm_resource_group.amiel_az_resource.location
   resource_group_name = azurerm_resource_group.amiel_az_resource.name
-  app_service_plan_id = azurerm_app_service_plan.azure_amiel_serice_plan.id
+  app_service_plan_id = azurerm_app_service_plan.azure_amiel_service_plan.id
   version             = "~3"
 
   app_settings = {
@@ -57,15 +48,44 @@ resource "azurerm_function_app" "amiel_az_func_app" {
     "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = azurerm_storage_account.amiel_storage.primary_connection_string
     "WEBSITE_CONTENTSHARE"                     = "azure-webjobs-hosts"
     "AzureWebJobsDashboard"                    = azurerm_storage_account.amiel_storage.primary_connection_string
+    "storage_account_name"                     = azurerm_storage_account.amiel_storage.name
+    "storage_account_access_key"               = azurerm_storage_account.amiel_storage.primary_access_key
   }
 
-  storage_account_access_key = "2kLa2It+zMlqn+lsV9bdyu/DGEsW/kiHRICSkasAm8IuH1nasNgc/1gw0lj78ScTPCk9ilQ1wcvx+ASt5hRCYg=="
-  storage_account_name       = "amielstorageaccount"
+  identity {
+    type = "SystemAssigned"
+  }
+  storage_account_name       = azurerm_storage_account.amiel_storage.name
+  storage_account_access_key = azurerm_storage_account.amiel_storage.primary_access_key
 }
+
+resource "azurerm_role_assignment" "amiel_role_assignment" {
+  scope                = azurerm_storage_account.amiel_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_function_app.amiel_az_func_app.identity[0].principal_id
+}
+
+
 
 resource "azurerm_private_dns_zone" "amiel_dns_zone" {
   name                = "privatelink.azurewebsites.net"
   resource_group_name = azurerm_resource_group.amiel_az_resource.name
+}
+
+
+
+resource "azurerm_private_endpoint" "amiel_private_endpoint" {
+  name                  = "amiel-private-endpoint"
+  location              = azurerm_resource_group.amiel_az_resource.location
+  resource_group_name   = azurerm_resource_group.amiel_az_resource.name
+  subnet_id             = azurerm_subnet.amiel_subnet.id
+
+  private_service_connection {
+    name                       = azurerm_function_app.amiel_az_func_app.name
+    is_manual_connection       = false
+    private_connection_resource_id = azurerm_function_app.amiel_az_func_app.id
+    subresource_names          = ["sites"]
+  }
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "amiel_dns_vnet_link" {
@@ -76,29 +96,14 @@ resource "azurerm_private_dns_zone_virtual_network_link" "amiel_dns_vnet_link" {
   registration_enabled  = true
 }
 
-resource "azurerm_private_endpoint" "amiel_private_endpoint" {
-  name                  = "amiel-private-endpoint"
+resource "azurerm_private_endpoint" "storage_private_endpoint" {
+  name                  = "storage-private-endpoint"
   location              = azurerm_resource_group.amiel_az_resource.location
   resource_group_name   = azurerm_resource_group.amiel_az_resource.name
   subnet_id             = azurerm_subnet.amiel_subnet.id
 
   private_service_connection {
-    name                       = "amiel-private-connection"
-    is_manual_connection       = false
-    private_connection_resource_id = azurerm_function_app.amiel_az_func_app.id
-    subresource_names          = ["sites"]
-  }
-}
-
-
-resource "azurerm_private_endpoint" "amiel_storage_endpoint" {
-  name                  = "amiel-storage-endpoint"
-  location              = azurerm_resource_group.amiel_az_resource.location
-  resource_group_name   = azurerm_resource_group.amiel_az_resource.name
-  subnet_id             = azurerm_subnet.amiel_subnet.id
-
-  private_service_connection {
-    name                       = "amiel-storage-connection"
+    name                       = azurerm_storage_account.amiel_storage.name
     is_manual_connection       = false
     private_connection_resource_id = azurerm_storage_account.amiel_storage.id
     subresource_names          = ["blob"]
@@ -106,6 +111,33 @@ resource "azurerm_private_endpoint" "amiel_storage_endpoint" {
 }
 
 
+output "resource_group_name" {
+  value = azurerm_resource_group.amiel_az_resource.name
+}
+
+output "virtual_network_name" {
+  value = azurerm_virtual_network.amiel_vpn.name
+}
+
+output "subnet_name" {
+  value = azurerm_subnet.amiel_subnet.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.amiel_storage.name
+}
+
+output "app_service_plan_name" {
+  value = azurerm_app_service_plan.azure_amiel_service_plan.name
+}
+
+output "function_app_name" {
+  value = azurerm_function_app.amiel_az_func_app.name
+}
+
+output "private_dns_zone_name" {
+  value = azurerm_private_dns_zone.amiel_dns_zone.name
+}
 
 resource "azurerm_monitor_diagnostic_setting" "amiel_monitor_diag" {
   name               = "my-diagnostics"
